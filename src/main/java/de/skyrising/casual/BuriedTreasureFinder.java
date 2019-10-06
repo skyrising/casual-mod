b999ab624179ab1ab8eb5e2a7ab4b972506250a7
Simon Wanner <skyrising@pvpctutorials.de>
2019-10-06 23:50:06 +0200

    Oops, copy-paste fail

--- src/main/java/de/skyrising/casual/BuriedTreasureFinder.java
+++ src/main/java/de/skyrising/casual/BuriedTreasureFinder.java
@@ -1,25 +1,15 @@
 package de.skyrising.casual;
 
-import com.mojang.datafixers.util.Either;
-import fi.dy.masa.malilib.util.IntBoundingBox;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.MobSpawnerBlockEntity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.feature.DungeonFeature;
+import net.minecraft.world.gen.feature.BuriedTreasureFeature;
 import net.minecraft.world.gen.feature.Feature;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.Optional;
 
-public class BuriedTreasureFinder implements StructureFinder<DungeonFeature> {
+public class BuriedTreasureFinder implements StructureFinder<BuriedTreasureFeature> {
     @Override
     public boolean isStartBlock(World world, BlockPos pos, @Nullable BlockEntity blockEntity) {
         Biome biome = world.getBiome(pos);
@@ -30,7 +20,7 @@ public class BuriedTreasureFinder implements StructureFinder<DungeonFeature> {
     }
 
     @Override
-    public DungeonFeature getFeature() {
-        return (DungeonFeature) Feature.MONSTER_ROOM;
+    public BuriedTreasureFeature getFeature() {
+        return (BuriedTreasureFeature) Feature.BURIED_TREASURE;
     }
 }
